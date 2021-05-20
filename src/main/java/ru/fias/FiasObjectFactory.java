288789961c26e09d837cf499f1297cc3838711d5
bek <bek.andrey@gmail.com>
2021-05-20 10:49:53 +0700

    add params files

--- src/main/java/ru/fias/FiasObjectFactory.java
+++ src/main/java/ru/fias/FiasObjectFactory.java
@@ -19,18 +19,18 @@ public class FiasObjectFactory {
             case "AS_HOUSES":
                 fiasClass = House.class;
                 break;
-//            case "AS_HOUSES_PARAMS":
-//                fiasClass = HouseParam.class;
-//                break;
+            case "AS_HOUSES_PARAMS":
+                fiasClass = HouseParam.class;
+                break;
             case "AS_ADM_HIERARCHY":
                 fiasClass = AdmHierarchy.class;
                 break;
             case "AS_MUN_HIERARCHY":
                 fiasClass = MunHierarchy.class;
                 break;
-//            case "AS_ADDR_OBJ_PARAMS":
-//                fiasClass = AddrObjParam.class;
-//                break;
+            case "AS_ADDR_OBJ_PARAMS":
+                fiasClass = AddrObjParam.class;
+                break;
             case "AS_OBJECT_LEVELS":
                 fiasClass = ObjectLevel.class;
                 break;
